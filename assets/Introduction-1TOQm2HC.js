952b220c90439191eaec1d90281edd63625645ff
drivardxrm <38399134+drivardxrm@users.noreply.github.com>
2026-09-05 11:40:54 +0000

    Deploying to gh-pages from  @ 028a382998cb09c1388ea003e47bec734377917d 🚀

---
+++ assets/Introduction-1TOQm2HC.js
@@ -0,0 +1,113 @@
+import{j as e}from"./index-66Y5VBdu.js";import{u as t,M as o}from"./blocks-BLwNjCpk.js";import"./preload-helper-C1FmrZbK.js";import"./iframe-VLHW0-V7.js";function r(i){const n={h1:"h1",p:"p",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Introduction"}),`
+`,e.jsx("style",{children:`
+  .subheading {
+    --mediumdark: '#999999';
+    font-weight: 900;
+    font-size: 13px;
+    color: #999;
+    letter-spacing: 6px;
+    line-height: 24px;
+    text-transform: uppercase;
+    margin-bottom: 12px;
+    margin-top: 40px;
+  }
+
+  .link-list {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr;
+    row-gap: 10px;
+  }
+
+  @media (min-width: 620px) {
+    .link-list {
+      row-gap: 20px;
+      column-gap: 20px;
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media all and (-ms-high-contrast:none) {
+  .link-list {
+      display: -ms-grid;
+      -ms-grid-columns: 1fr 1fr;
+      -ms-grid-rows: 1fr 1fr;
+    }
+  }
+
+  .link-item {
+    display: block;
+    padding: 20px 30px 20px 15px;
+    border: 1px solid #00000010;
+    border-radius: 5px;
+    transition: background 150ms ease-out, border 150ms ease-out, transform 150ms ease-out;
+    color: #333333;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .link-item:hover {
+    border-color: #1EA7FD50;
+    transform: translate3d(0, -3px, 0);
+    box-shadow: rgba(0, 0, 0, 0.08) 0 3px 10px 0;
+  }
+
+  .link-item:active {
+    border-color: #1EA7FD;
+    transform: translate3d(0, 0, 0);
+  }
+
+  .link-item strong {
+    font-weight: 700;
+    display: block;
+    margin-bottom: 2px;
+  }
+
+  .link-item img {
+    height: 40px;
+    width: 40px;
+    margin-right: 15px;
+    flex: none;
+  }
+
+  .link-item span {
+    font-size: 14px;
+    line-height: 20px;
+  }
+
+  .tip {
+    display: inline-block;
+    border-radius: 1em;
+    font-size: 11px;
+    line-height: 12px;
+    font-weight: 700;
+    background: #E7FDD8;
+    color: #66BF3C;
+    padding: 4px 12px;
+    margin-right: 10px;
+    vertical-align: top;
+  }
+
+  .tip-wrapper {
+    font-size: 13px;
+    line-height: 20px;
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+
+  .tip-wrapper code {
+    font-size: 12px;
+    display: inline-block;
+  }
+`}),`
+`,e.jsx(n.h1,{id:"lookupdropdown-pcf-storybook",children:"LookupDropdown PCF Storybook"}),`
+`,e.jsx("img",{src:"https://raw.githubusercontent.com/drivardxrm/LookupDropdown.PCF/main/images/lookupdropdown_V2.png"}),`
+`,e.jsx("div",{className:"tip-wrapper",children:e.jsxs(n.p,{children:[e.jsx("span",{className:"tip",children:"Tip"}),`Browse example stories now by navigating to them in the sidebar.
+View their code in the `,e.jsx("code",{children:"stories"})," directory to learn how they work."]})}),`
+`,e.jsx("div",{className:"tip-wrapper",children:e.jsxs(n.p,{children:[e.jsx("span",{className:"tip",children:"Acknowlegment"})," Stories implemented with the help of the ",e.jsx("code",{children:"ComponentFramework Mock"})," library by ",e.jsx("a",{href:"https://shko.online/",target:"_blank",children:"Shko Online"})]})}),`
+`,e.jsx("div",{className:"subheading",children:"Links"}),`
+`,e.jsxs("div",{className:"link-list",children:[e.jsxs("a",{className:"link-item",href:"https://github.com/drivardxrm/LookupDropdown.PCF",target:"_blank",children:[e.jsx("img",{src:"https://pngimg.com/uploads/github/github_PNG20.png",alt:"plugin"}),e.jsx("span",{children:e.jsxs(n.p,{children:[e.jsx("strong",{children:"GitHub Repo"}),`
+LookupDropdown.PCF`]})})]}),e.jsxs("a",{className:"link-item",href:"https://pcf.gallery/lookup-dropdown-pcf/",target:"_blank",children:[e.jsx("img",{src:"https://pcf.gallery/assets/images/logo.png",alt:"PCF Gallery"}),e.jsx("span",{children:e.jsxs(n.p,{children:[e.jsx("strong",{children:"PCF Gallery"}),`
+LookupDropdown PCF`]})})]}),e.jsxs("a",{className:"link-item",href:"https://github.com/Shko-Online/ComponentFramework-Mock",target:"_blank",children:[e.jsx("img",{src:"https://avatars.githubusercontent.com/u/103819795?s=200&v=4",alt:"Shko Online"}),e.jsx("span",{children:e.jsxs(n.p,{children:[e.jsx("strong",{children:"Shko Online"}),`
+ComponentFramework Mock`]})})]})]})]})}function d(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(r,{...i})}):r(i)}export{d as default};
+//# sourceMappingURL=Introduction-1TOQm2HC.js.map
---
+++ assets/formatter-L2YTDE3Q-CQS5Mawk.js
@@ -0,0 +1,2 @@
+import{r as m}from"./blocks-BLwNjCpk.js";import{d as e,b as o}from"./iframe-VLHW0-V7.js";import"./preload-helper-C1FmrZbK.js";import"./index-66Y5VBdu.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
+//# sourceMappingURL=formatter-L2YTDE3Q-CQS5Mawk.js.map
